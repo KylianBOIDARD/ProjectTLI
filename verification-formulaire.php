7ba19ae9029621ee68de4d747d546903ffab6128
FaresBouamar <56865061+FaresBouamar@users.noreply.github.com>
2021-03-03 16:02:52 +0100

    Add files via upload

--- verification-formulaire.php
+++ verification-formulaire.php
@@ -1,40 +1,21 @@
-    <!DOCTYPE html>
-<html>
-  <?php
-      $bdd = new PDO('pgsql:host=localhost;port=5432;dbname=acudb', 'postgres-tli', 'tli');
+<meta charset="utf-8" />
+<?php
+ 
+$bdd = new PDO('pgsql:host=localhost;port=5432;dbname=acudb', 'postgres-tli', 'tli');
+ 
+$terme = $bdd->query('SELECT "desc" FROM patho');
+if(isset($_GET['terme']) AND !empty($_GET['terme'])) {
+   $q = htmlspecialchars($_GET['terme']);
+   $terme = $bdd->query('SELECT * FROM patho WHERE "desc" LIKE \'%'.$q.'%\'');
+   //var_dump($terme);
+   //var_dump($bdd->errorInfo());
+   }
 
-      if (isset($_GET["rechercher"]) AND $_GET["rechercher"] == "Rechercher") {
-      $_GET["terme"] = htmlspecialchars($_GET["terme"]); //pour sécuriser le formulaire contre les failles html
-      $terme = $_GET["terme"];
-      $terme = trim($terme); //pour supprimer les espaces dans la requête de l'internaute
-      $terme = strip_tags($terme); //pour supprimer les balises html dans la requête
+?>
 
-      }
-
-      if (isset($terme))
-      {
-      $terme = strtolower($terme);
-      $select_terme = $bdd->prepare("SELECT * FROM patho WHERE * LIKE ?");
-      $select_terme->execute(array("%".$terme."%"));
-      }
-      else
-      {
-      $message = "Vous devez entrer votre requete dans la barre de recherche";
-      }
-  ?>
-
-
- <head>
-  <meta charset = "utf-8" >
-  <title>Les résultats de recherche</title>
- </head>
- <body>
-  <?php
-  while($terme_trouve = $select_terme->fetch())
-  {
-   echo "<div><p> ".$terme_trouve['desc']."</p>";
-  }
-  $select_terme->closeCursor();
-   ?>
- </body>
-</html>
+</form>
+   <ul>
+   <?php while($a = $terme->fetch()) { ?>
+      <li><?= $a["desc"] ?></li>
+   <?php } ?>
+   </ul>
