0885794835eb03b76db4b0be2a484107b51a819a
KylianBOIDARD <kylian.boidard@gmail.com>
2021-03-03 14:32:36 +0100

    ajout index.php dans navbar

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ $maladies = $bd->query('SELECT * FROM patho');
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                <a class="nav-link" href="./index.html">Acceuil <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="./index.php">Acceuil <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
                 <a class="nav-link active" href="./informations.html">Informations</a>
